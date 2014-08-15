caa34976e457fefb7cbf0bf9d394b5acfc148db5
Jan Iwanow <iwanow.jan@gmail.com>
2014-08-15 20:28:40 +1100

    rewrote blur animation on images and iframes using jquery waypoints and CSS 3 filters

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,52 +1,97 @@
 $(document).ready(function() {
     $('.link--preview').livePreview();
 
-    blurFilter('.js-blurrr-image', '.js-blurrr-text');
+    addBlurEffects({
+        image: '.js-blurrr-image',
+        text: '.js-blurrr-text',
+        maxBlur: 15
+    });
 });
 
-function blurFilter(image, text){
-    var $image = $(image);
-    var $text = $(text);
-    var $body = $('body');
+function addBlurEffects(options){
+    function scrollHandler(evt){
+        var offset, coefficient, blur, opacity,
+            windowScroll = $(window).scrollTop();
 
-    $image.appear();
+        if (evt.data.imageOffsetTop <  $(window).height() && windowScroll < evt.data.imageOffsetTop){
+            blur = Math.floor(evt.data.initialBlur - windowScroll / 10);
+            opacity = 1 - windowScroll / 100;
 
-    $body.on('appear', image, function(evt, $img){
-        var scroll = $(window).scrollTop();
-        var imageOffsetTop = $img.offset().top;
-        var imageHeight = $img.height();
-        var triggeringOffset = (imageOffsetTop - imageHeight * 2);
-        var triggeringBottomOffset = (imageOffsetTop + imageHeight * 2);
-
-        if (scroll >= triggeringOffset && scroll <= triggeringBottomOffset){
-            var coeff = (scroll - imageOffsetTop) / 5;
-
-            if (coeff > 0) { coeff = 0 }
-            if (coeff < -15) { coeff = -15 }
-
-            var imageBlur = -coeff;
-            var imageBlurValue = 'blur(' + imageBlur + 'px)';
-
-            $img.css({
-                'filter'         : imageBlurValue,
-                '-webkit-filter' : imageBlurValue,
-                '-moz-filter'    : imageBlurValue,
-                '-o-filter'      : imageBlurValue,
-                '-ms-filter'     : imageBlurValue
-            });
-
-            var textBlur = 15 - imageBlur;
-            var textBlurValue = 'blur(' + textBlur + 'px)';
-            var textOpacity = 1 - (textBlur / 15);
-
-            $text.css({
-                'filter'         : textBlurValue,
-                '-webkit-filter' : textBlurValue,
-                '-moz-filter'    : textBlurValue,
-                '-o-filter'      : textBlurValue,
-                '-ms-filter'     : textBlurValue,
-                'opacity'        : textOpacity
-            });
+            console.log(blur, opacity);
         }
+        else {
+            // some magic...
+            offset = (evt.data.imageOffsetTop - windowScroll) * 10;
+
+            coefficient = (offset / (evt.data.imageHalfHeight / evt.data.initialBlur));
+
+            blur = (evt.data.initialBlur + coefficient) / 10;
+            opacity = coefficient / 100;
+        }
+
+        if (blur < 0) {
+            blur = 0;
+        }
+
+        if (blur > options.maxBlur) {
+            blur = options.maxBlur;
+        }
+
+        if (opacity > 1) { opacity = 1; }
+        if (opacity < 0) { opacity = 0; }
+
+        //evt.data.filter.setStdDeviation(blur, blur);
+
+        var blurString = 'blur(' + blur + 'px)';
+
+        evt.data.$img.css({
+            'filter'         : blurString,
+            '-webkit-filter' : blurString,
+            '-moz-filter'    : blurString,
+            '-o-filter'      : blurString,
+            '-ms-filter'     : blurString
+        });
+
+        evt.data.$text.css({ opacity: opacity });
+    }
+
+    var $image = $(options.image);
+
+    $image.each(function(){
+        var $this = $(this);
+        var imageOffsetTop = $this.offset().top;
+        var imageHeight = $this.height();
+        var imageHalfHeight = imageHeight / 2;
+
+        var $text = $this.nextAll(options.text + ':first');
+
+       // var filter = $('> filter', $this).get(0).firstElementChild;
+        var initialBlur = options.maxBlur;//parseInt(filter.getAttribute('stdDeviation'));
+
+        function waypointCallback(direction){
+            var data = {
+                direction: direction,
+                imageOffsetTop: imageOffsetTop,
+                imageHalfHeight: imageHalfHeight,
+                initialBlur: initialBlur,
+                //filter: filter,
+                $img: $this,
+                $text: $text
+            };
+
+            $(window).on('scroll', data, scrollHandler);
+        }
+
+        var offsetTop;
+
+        if (imageOffsetTop < $(window).height() / 2){
+            offsetTop = imageOffsetTop;
+        }
+        else {
+            offsetTop = imageHalfHeight;
+        }
+
+        $this.waypoint(waypointCallback, { offset: offsetTop });
+        $this.waypoint(waypointCallback, { offset: -imageHalfHeight });
     });
 }
