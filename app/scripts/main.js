ff72d8543d3574fe9fa3fe23846b594330e73ad6
Jan Iwanow <iwanow.jan@gmail.com>
2014-08-27 02:54:30 +1100

    changed animation behaviour

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,44 +3,40 @@ $(document).ready(function() {
 
     addBlurEffects({
         image: '.js-blurrr-image',
-        text: '.js-blurrr-text',
-        maxBlur: 5
+        text: '.js-blurrr-text'
     });
 });
 
 function addBlurEffects(options){
-    var wrappedElementsString = '.header, .header + div';
-    var $wrappedElements = $(wrappedElementsString);
-    var wrapperString = '<div class="js-fixed-wrapper fixed-wrapper"></div>';
+    var wrappedElementsString = '.main > *',
+        $wrappedElements = $(wrappedElementsString),
+        wrapperString = '<div class="js-fixed-wrapper fixed-wrapper"></div>',
+        $body = $('body'),
+        bodyHeight = $body.height(),
+        emptyFillString = '<div id="bodyEmptyFill"></div>';
+
+    $body.append(emptyFillString);
+
+    var $emptyFill = $('#bodyEmptyFill');
+
+    $emptyFill.height(bodyHeight);
 
     $wrappedElements.wrapAll(wrapperString);
 
     var $fixedWrapper = $('.js-fixed-wrapper');
 
     function scrollHandler(evt){
-        var offset, coefficient, blur, opacity,
+        var offset, coefficient, opacity,
             windowScroll = $(window).scrollTop();
 
         if (evt.data.imageOffsetTop <  $(window).height() && windowScroll < evt.data.imageOffsetTop){
-            blur = Math.floor(evt.data.initialBlur - windowScroll / 10);
             opacity = 1 - windowScroll / 100;
         }
         else {
             // some magic...
             offset = (evt.data.imageOffsetTop - windowScroll) * 10;
-
             coefficient = (offset / (evt.data.imageHalfHeight / evt.data.initialBlur));
-
-            blur = (evt.data.initialBlur + coefficient) / 10;
             opacity = coefficient / 100;
-        }
-
-        if (blur <= 0) {
-            blur = 0;
-        }
-
-        if (blur >= options.maxBlur) {
-            blur = options.maxBlur;
         }
 
         if (opacity >= 1) {
@@ -55,18 +51,6 @@ function addBlurEffects(options){
             evt.data.$text.trigger('victore.text.hidden');
         }
 
-        //evt.data.filter.setStdDeviation(blur, blur);
-
-        var blurString = 'blur(' + blur + 'px)';
-
-        evt.data.$img.css({
-            'filter'         : blurString,
-            '-webkit-filter' : blurString,
-            '-moz-filter'    : blurString,
-            '-o-filter'      : blurString,
-            '-ms-filter'     : blurString
-        });
-
         evt.data.$text.css({ opacity: opacity });
         evt.data.$shadowed.css({ opacity: opacity });
     }
@@ -74,6 +58,7 @@ function addBlurEffects(options){
     function textShownCallback(evt){
         $wrappedElements.prependTo('.main');
         $('.js-fixed-wrapper').remove();
+        $emptyFill.show();
         $wrappedElements.wrapAll(wrapperString);
 
         evt.data.$text.one('victore.text.hidden', null, { $text: evt.data.$text }, textHiddenCallback);
@@ -82,6 +67,7 @@ function addBlurEffects(options){
     function textHiddenCallback(evt){
         $wrappedElements.prependTo('.main');
         $('.js-fixed-wrapper').remove();
+        $emptyFill.hide();
 
         evt.data.$text.one('victore.text.shown', null, { $text: evt.data.$text }, textShownCallback);
     }
@@ -100,9 +86,6 @@ function addBlurEffects(options){
         $text.one('victore.text.shown', null, textData, textShownCallback);
         $text.one('victore.text.hidden', null, textData, textHiddenCallback);
 
-       // var filter = $('> filter', $this).get(0).firstElementChild;
-        var initialBlur = options.maxBlur;//parseInt(filter.getAttribute('stdDeviation'));
-
         var $thisParent = $this.parent();
         $('<div class="js-shadowed shadowed"></div>').appendTo($thisParent);
 
@@ -111,7 +94,6 @@ function addBlurEffects(options){
                 direction: direction,
                 imageOffsetTop: imageOffsetTop,
                 imageHalfHeight: imageHalfHeight,
-                initialBlur: initialBlur,
                 //filter: filter,
                 $img: $this,
                 $text: $text,
