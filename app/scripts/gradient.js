47d7468fad9ca49a188841a77bfd52c75e140496
Jan Iwanow <iwanow.jan@gmail.com>
2014-08-27 03:29:57 +1100

    %14 - added standard gradient definition for further browsers support and IE10-11

--- app/scripts/gradient.js
+++ app/scripts/gradient.js
@@ -2,13 +2,14 @@ jQuery(function($){
     /* home page */
 
     /* color transitions */
-    var colors = new Array(
+    var colors = [
         [62,35,255],
         [60,255,60],
         [255,35,98],
         [45,175,230],
         [255,0,255],
-        [255,128,0]);
+        [255,128,0]
+    ];
 
     var step = 0;
 
@@ -34,16 +35,16 @@ jQuery(function($){
         var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
         var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
         var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-        var color1 = "#"+((r1 << 16) | (g1 << 8) | b1).toString(16);
+        var color1 = '#'+((r1 << 16) | (g1 << 8) | b1).toString(16);
 
         var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
         var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
         var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-        var color2 = "#"+((r2 << 16) | (g2 << 8) | b2).toString(16);
+        var color2 = '#'+((r2 << 16) | (g2 << 8) | b2).toString(16);
 
-        $btn.css({
-            background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
-            background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
+        $btn.css({ background: '-webkit-gradient(linear, left top, right top, from('+color1+'), to('+color2+'))' })
+            .css({ background: '-moz-linear-gradient(left, '+color1+' 0%, '+color2+' 100%)' })
+            .css({ background: 'linear-gradient(to left, '+color1+' 0%, '+color2+' 100%)' });
 
         step += gradientSpeed;
 
--- dist/assets/0.7.2/scripts/gradient.js
+++ dist/assets/0.7.2/scripts/gradient.js
@@ -41,9 +41,9 @@ jQuery(function($){
         var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
         var color2 = "#"+((r2 << 16) | (g2 << 8) | b2).toString(16);
 
-        $btn.css({
-            background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
-            background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
+        $btn.css({ background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))" })
+            .css({ background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)" })
+            .css({ background: "linear-gradient(to left, )" });
 
         step += gradientSpeed;
 
