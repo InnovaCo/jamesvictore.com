880aa56bc9c60712054aa8ad46e0b63d8af45db9
Jan Iwanow <iwanow.jan@gmail.com>
2014-08-27 22:11:26 +1100

    rewrote animation completely

--- dist/assets/0.7.2/scripts/img-anim.js
+++ dist/assets/0.7.2/scripts/img-anim.js
@@ -1,116 +1,38 @@
 $(document).ready(function() {
-    function addBlurEffects(options){
-        var wrappedElementsString = '.main > *',
-            $wrappedElements = $(wrappedElementsString),
-            wrapperString = '<div class="js-fixed-wrapper fixed-wrapper"></div>',
-            $body = $('body'),
-            bodyHeight = $body.height(),
-            emptyFillString = '<div id="bodyEmptyFill"></div>';
+    var $window = $(window);
+    var $shadow = $('#shadow');
+    var $text = $('#text');
+    var opacity = 1;
+    var mathCoeff = 0.07;
+    var eventsString = 'mousewheel.victore touchmove.victore scroll.victore';
 
-        $body.append(emptyFillString);
+    var scrollCallback = function(e){
+        if (opacity <= 0 || $window.scrollTop() > 50){
+            opacity = 0.0;
 
-        var $emptyFill = $('#bodyEmptyFill');
+            $text.css({ opacity: opacity });
+            $shadow.css({ opacity: opacity });
 
-        $emptyFill.height(bodyHeight);
+            $window.off(eventsString);
 
-        $wrappedElements.wrapAll(wrapperString);
-
-        var $fixedWrapper = $('.js-fixed-wrapper');
-
-        function scrollHandler(evt){
-            var offset, coefficient, opacity,
-                windowScroll = $(window).scrollTop();
-
-            if (evt.data.imageOffsetTop <  $(window).height() && windowScroll < evt.data.imageOffsetTop){
-                opacity = 1 - windowScroll / 100;
-            }
-            else {
-                // some magic...
-                offset = (evt.data.imageOffsetTop - windowScroll) * 10;
-                coefficient = (offset / (evt.data.imageHalfHeight / evt.data.initialBlur));
-                opacity = coefficient / 100;
-            }
-
-            if (opacity >= 1) {
-                opacity = 1;
-
-                evt.data.$text.trigger('victore.text.shown');
-            }
-
-            if (opacity < 0) {
-                opacity = 0;
-
-                evt.data.$text.trigger('victore.text.hidden');
-            }
-
-            evt.data.$text.css({ opacity: opacity });
-            evt.data.$shadowed.css({ opacity: opacity });
+            return;
         }
 
-        function textShownCallback(evt){
-            $wrappedElements.prependTo('.main');
-            $('.js-fixed-wrapper').remove();
-            $emptyFill.show();
-            $wrappedElements.wrapAll(wrapperString);
+        if (opacity > 1){ opacity = 1; }
 
-            evt.data.$text.one('victore.text.hidden', null, { $text: evt.data.$text }, textHiddenCallback);
+        if (opacity >= 0 && opacity <= 1){
+            var down = (e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0);
+
+            opacity = (down ? opacity-mathCoeff : opacity+mathCoeff);
+
+            $text.css({ opacity: opacity });
+            $shadow.css({ opacity: opacity });
         }
 
-        function textHiddenCallback(evt){
-            $wrappedElements.prependTo('.main');
-            $('.js-fixed-wrapper').remove();
-            $emptyFill.hide();
-
-            evt.data.$text.one('victore.text.shown', null, { $text: evt.data.$text }, textShownCallback);
+        if (opacity >= 0){
+            return false;
         }
+    };
 
-        var $image = $(options.image);
-
-        $image.each(function(){
-            var $this = $(this);
-            var imageOffsetTop = $this.offset().top;
-            var imageHeight = $this.height();
-            var imageHalfHeight = imageHeight / 2;
-
-            var $text = $this.nextAll(options.text + ':first');
-            var textData = { $text: $text };
-
-            $text.one('victore.text.shown', null, textData, textShownCallback);
-            $text.one('victore.text.hidden', null, textData, textHiddenCallback);
-
-            var $thisParent = $this.parent();
-            $('<div class="js-shadowed shadowed"></div>').appendTo($thisParent);
-
-            function waypointCallback(direction){
-                var data = {
-                    direction: direction,
-                    imageOffsetTop: imageOffsetTop,
-                    imageHalfHeight: imageHalfHeight,
-                    //filter: filter,
-                    $img: $this,
-                    $text: $text,
-                    $shadowed: $('.js-shadowed', $thisParent)
-                };
-
-                $(window).on('scroll', data, scrollHandler);
-            }
-
-            var offsetTop;
-
-            if (imageOffsetTop < $(window).height() / 2){
-                offsetTop = imageOffsetTop;
-            }
-            else {
-                offsetTop = imageHalfHeight;
-            }
-
-            $this.waypoint(waypointCallback, { offset: offsetTop });
-            $this.waypoint(waypointCallback, { offset: -imageHalfHeight });
-        });
-    }
-
-    addBlurEffects({
-        image: '.js-blurrr-image',
-        text: '.js-blurrr-text'
-    });
+    $window.on(eventsString, scrollCallback);
 });
