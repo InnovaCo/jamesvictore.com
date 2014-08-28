521e8ba7c5bb84b4179350ff436463f0f0593bdb
Jan Iwanow <iwanow.jan@gmail.com>
2014-08-28 21:30:28 +1100

    made animation to not block scroll

--- dist/assets/0.7.2/scripts/img-anim.js
+++ dist/assets/0.7.2/scripts/img-anim.js
@@ -1,18 +1,31 @@
 $(document).ready(function() {
-    var $window = $(window);
-    var $shadow = $('#shadow');
-    var $text = $('#text');
-    var opacity = 1;
-    var mathCoeff = 0.07;
+    var $window = $(window),
+        $hiddable = $('#text, #shadow'),
+        opacity = 1,
+        oldScroll = 0;
+
+    $window.scroll(function(){
+        var scroll = $window.scrollTop();
+
+        if (opacity > 1) { opacity = 1; }
+        if (opacity < 0) { opacity = 0; }
+
+        $hiddable.css({ opacity: (1 - scroll / 100) });
+
+        oldScroll = scroll;
+    });
+
+    /*var opacity = 1;
+    var mathCoeff = 0.001;
     var eventsString = 'mousewheel.victore touchmove.victore scroll.victore';
 
     var scrollCallback = function(e){
+        console.log(Math.abs(e.originalEvent.wheelDelta));
+
         if (opacity <= 0 || $window.scrollTop() > 50){
             opacity = 0.0;
 
-            $text.css({ opacity: opacity });
-            $shadow.css({ opacity: opacity });
-
+            $hiddable.css({ opacity: opacity });
             $window.off(eventsString);
 
             return;
@@ -23,10 +36,10 @@ $(document).ready(function() {
         if (opacity >= 0 && opacity <= 1){
             var down = (e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0);
 
+            mathCoeff = Math.abs(e.originalEvent.wheelDelta || 0.5) * mathCoeff;
             opacity = (down ? opacity-mathCoeff : opacity+mathCoeff);
 
-            $text.css({ opacity: opacity });
-            $shadow.css({ opacity: opacity });
+            $hiddable.css({ opacity: opacity });
         }
 
         if (opacity >= 0){
@@ -35,4 +48,5 @@ $(document).ready(function() {
     };
 
     $window.on(eventsString, scrollCallback);
+    */
 });
