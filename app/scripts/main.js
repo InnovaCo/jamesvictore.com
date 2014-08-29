96357d2b662607aca12d37f2fa04259386524ae7
iKorso <korsoq12@gmail.com>
2014-08-29 16:40:48 +0400

    some fixes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,23 @@
-$(document).ready(function() {
-    $('.link--preview').livePreview();
+$(function() {
+	$('.link--preview').livePreview();
+
+	// video frame resize
+
+	var $videoBlock = $('.js-video-wrapper');
+
+	function videoResize(e) {
+		var videoHeight = e.height(),
+			videoWidth = e.width();
+		videoHeight = videoWidth / 1.77;
+		e.css({
+			'height':videoHeight
+		});
+	}
+
+	videoResize($videoBlock);
+
+	$(window).on('resize', function() {
+		videoResize($videoBlock);
+	});
+
 });
